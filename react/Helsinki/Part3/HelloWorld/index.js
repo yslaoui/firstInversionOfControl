4588cb196a89838def8096449dce1e65e5bb93cf
yslaoui <yslaouiyassine@gmail.com>
2023-02-23 22:23:27 +0100

    Adding route handlers for GET to /notes/:id and POST requests using the mongoDB database

--- react/Helsinki/Part3/HelloWorld/index.js
+++ react/Helsinki/Part3/HelloWorld/index.js
@@ -1,14 +1,14 @@
 
+//***** IMPORT STATEMENTS******** */
+//******************************* */
 require('dotenv').config() // this is for process.env to work
 const Note = require('./models/note')
 const http = require('http')
 const express = require('express')
 
 
-
-
-// ***** MIDDLEWARE
-
+//***** MIDDLEWARE ************** */
+//******************************* */
 
 // JSON parser middleware
 const app = express()
@@ -18,27 +18,54 @@ app.use(express.json()) // for turning data of content-type application/json int
 const cors = require('cors')
 app.use(cors())
 
-
 // Middleware allows express.js to work with static js front end files generated with npm run build
 app.use(express.static('build'))
 
 
-// **** Object literal that will be serverd as JSON
-
+//***** ROUTES FOR HANDLING HTTP REQUESTS ************** */
+//*********************************** */
+// The react page is in localshost/3001. 
+// The server listens to port 3001
+// The server serves the whole JSON data in localshost/3001/notes
+// The server serves each resource in localshost/3001/notes/:id
 
 
 app.get('/notes', (req, res) => {
+ // Find is a READ operation in CRUD 
  Note.find({}).then(result => {
    res.json(result)
  })
 })
 
-
-
-app.get('/', (req, res) => {
-   res.send('<p> Hello Guys</p>')
+app.get('/notes/:id', (req, res)=> {
+  // FindByID is a READ operation in CRUD
+  Note.findById(req.params.id).then((x)=>{
+    res.json(x)
+  })
 })
 
+app.post('/notes', (req, res)=> {
+  
+  if (req.body.content == undefined) {
+    return res.status(404).json({error:'content missing'})
+  }
+
+  const note = new Note({
+    content: req.body.content,
+    important: req.body.important || false
+  })
+
+  // save() is  a CREATE request in the database (CREATE, READ, UPDATE, DELETE)
+  note.save().then((x)=>{
+    res.json(x)
+  })
+  
+})
+
+
+
+//***** SERVER ORIGIN PORT ************** */
+//*************************************** */
 
 const PORT = process.env.PORT ||  3001
 app.listen(PORT, () => {
--- react/Helsinki/Part3/HelloWorld/models/note.js
+++ react/Helsinki/Part3/HelloWorld/models/note.js
@@ -7,12 +7,14 @@ const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-
+// SCHEMA
 const noteSchema = new mongoose.Schema({
  content: String,
  important: Boolean
 })
 
+
+// Modifying the toJSON method to delete the __v attribute, rename _id into id, and turn it into a string, because it is actually and object
 noteSchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
@@ -21,9 +23,5 @@ noteSchema.set('toJSON', {
     }
    })
 
-
-   
-
-
-
+// mongoose.model() creates a Javascript class representing the collection 'Note' that sits in the database. We export that Javascrit representation 
 module.exports = mongoose.model('Note', noteSchema)
